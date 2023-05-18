342434b66ab375683fa34d3e53b41f13a0b4f930
GarySmithAlvarezVenturo <Garyalvarez@outlook.it>
2023-05-18 19:34:25 +0200

    add footer-end

--- src/components/AFooter.vue
+++ src/components/AFooter.vue
@@ -43,66 +43,81 @@
     </section>
 
     <div class="footer-img">
-        <div class="contacts">
-            <div>
-                <div class="column">
-                    <ul>
-                        <li class="title">DC COMICS</li>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Comics</a></li>
-                        <li><a href="">Movies</a></li>
-                        <li><a href="">TV</a></li>
-                        <li><a href="">Games</a></li>
-                        <li><a href="">Videos</a></li>
-                        <li><a href="">News</a></li>
-                    </ul>
+      <div class="contacts">
+        <div>
+          <div class="column">
+            <ul>
+              <li class="title">DC COMICS</li>
+              <li><a href="">Characters</a></li>
+              <li><a href="">Comics</a></li>
+              <li><a href="">Movies</a></li>
+              <li><a href="">TV</a></li>
+              <li><a href="">Games</a></li>
+              <li><a href="">Videos</a></li>
+              <li><a href="">News</a></li>
+            </ul>
 
-                    <ul>
-                        <li class="title">SHOP</li>
-                        <li><a href="">Shop DC</a></li>
-                        <li><a href="">Shop DC Collectibles</a></li>
-                    </ul>
-                </div>
+            <ul>
+              <li class="title">SHOP</li>
+              <li><a href="">Shop DC</a></li>
+              <li><a href="">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
 
-                <div>
-                    <ul>
-                    <li class="title">DC</li>
-                    <li><a href="">Terms Of use</a></li>
-                    <li><a href="">Privacy policy(New)</a></li>
-                    <li><a href="">Ad Choices</a></li>
-                    <li><a href="">Advertising</a></li>
-                    <li><a href="">Jobs</a></li>
-                    <li><a href="">Subscriptions</a></li>
-                    <li><a href="">Talent Workshops</a></li>
-                    <li><a href="">CPSC Certificates</a></li>
-                    <li><a href="">Ratings</a></li>
-                    <li><a href="">Shop Help</a></li>
-                    <li><a href="">Contatc Us</a></li>
-                    </ul>
-                </div>
+          <div>
+            <ul>
+              <li class="title">DC</li>
+              <li><a href="">Terms Of use</a></li>
+              <li><a href="">Privacy policy(New)</a></li>
+              <li><a href="">Ad Choices</a></li>
+              <li><a href="">Advertising</a></li>
+              <li><a href="">Jobs</a></li>
+              <li><a href="">Subscriptions</a></li>
+              <li><a href="">Talent Workshops</a></li>
+              <li><a href="">CPSC Certificates</a></li>
+              <li><a href="">Ratings</a></li>
+              <li><a href="">Shop Help</a></li>
+              <li><a href="">Contatc Us</a></li>
+            </ul>
+          </div>
 
-                <div>
-                    <ul>
-                    <li class="title">SITES</li>
-                    <li><a href="">DC</a></li>
-                    <li><a href="">Mad Megazine</a></li>
-                    <li><a href="">DC Kids</a></li>
-                    <li><a href="">DC Universe</a></li>
-                    <li><a href="">DC Power Visa</a></li>
-                    </ul>
-                </div>
-            </div>
-        
-        
-            <div>
-            <img src="../assets/vue-dc-comics-1/img/dc-logo-bg.png" alt="" />
-            
-            </div>
+          <div>
+            <ul>
+              <li class="title">SITES</li>
+              <li><a href="">DC</a></li>
+              <li><a href="">Mad Megazine</a></li>
+              <li><a href="">DC Kids</a></li>
+              <li><a href="">DC Universe</a></li>
+              <li><a href="">DC Power Visa</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <img src="../assets/vue-dc-comics-1/img/dc-logo-bg.png" alt="" />
+        </div>
       </div>
     </div>
 
     <div class="footer-end">
-      <section class="socials"></section>
+      <section class="socials">
+        <button>SIGN-UP NOW!</button>
+
+        <div>
+          <h3>FOLLOW US</h3>
+          <img src="../assets/vue-dc-comics-1/img/footer-facebook.png" alt="" />
+          <img src="../assets/vue-dc-comics-1/img/footer-twitter.png" alt="" />
+          <img src="../assets/vue-dc-comics-1/img/footer-youtube.png" alt="" />
+          <img
+            src="../assets/vue-dc-comics-1/img/footer-pinterest.png"
+            alt=""
+          />
+          <img
+            src="../assets/vue-dc-comics-1/img/footer-periscope.png"
+            alt=""
+          />
+        </div>
+      </section>
     </div>
   </footer>
 </template>
@@ -132,7 +147,6 @@
     }
 
     p {
-      padding-inline: 1.5rem;
       color: white;
       white-space: nowrap;
     }
@@ -182,6 +196,33 @@
 
 .footer-end {
   background-color: rgb(48, 48, 48);
-  height: 8rem;
+  height: 12rem;
+}
+
+.socials {
+  max-width: 1200px;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 10rem;
+
+  button {
+    background-color: transparent;
+    color: white;
+    border: 2px solid rgb(12, 119, 234);
+    padding: 0.7rem;
+    font-size: 1rem;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    h3 {
+      color: rgb(12, 119, 234);
+    }
+  }
 }
 </style>
